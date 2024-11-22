b4915888cf04905f315df9f45eb3a440b926ea97
Lockynbr <pauloluz566@gmail.com>
2024-11-21 21:36:54 -0300

    excluindo alunos

--- src/pages/Students/index.js
+++ src/pages/Students/index.js
@@ -1,13 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { get } from 'lodash';
-import { FaUserCircle, FaEdit, FaWindowClose } from 'react-icons/fa';
+import {
+  FaUserCircle,
+  FaEdit,
+  FaWindowClose,
+  FaExclamation,
+} from 'react-icons/fa';
 
 import { Container } from '../../styles/GlobalStyles';
 import { StudentContainer, ProfilePicture } from './styled';
 import axios from '../../services/axios';
 
 import Loading from '../../components/Loading';
+import { toast } from 'react-toastify';
 
 export default function Students() {
   const [alunos, setAlunos] = useState([]);
@@ -24,12 +30,40 @@ export default function Students() {
     getData();
   }, []);
 
+  const handleDeleteAsk = (e) => {
+    e.preventDefault();
+    const exclamation = e.currentTarget.nextSibling;
+    exclamation.setAttribute('display', 'block');
+    e.currentTarget.remove();
+  };
+
+  const handleDelete = async (e, id, index) => {
+    e.persist();
+    try {
+      setIsLoading(true);
+      await axios.delete(`/alunos/${id}`);
+      const newStudents = [...alunos];
+      newStudents.splice(index, 1);
+      setAlunos(newStudents);
+      setIsLoading(false);
+    } catch (err) {
+      const status = get(err, 'response.data.errors', 0);
+
+      if (status === 401) {
+        toast.error('Login required!');
+      } else {
+        toast.error('An error occurred while deleting student');
+        setIsLoading(false);
+      }
+    }
+  };
+
   return (
     <Container>
       <Loading isLoading={isLoading} />
       <h1>Students</h1>
       <StudentContainer>
-        {alunos.map((aluno) => (
+        {alunos.map((aluno, index) => (
           <div key={String(aluno.id)}>
             <ProfilePicture>
               {get(aluno, 'Photos[0].url', false) ? (
@@ -45,9 +79,16 @@ export default function Students() {
             <Link to={`/student/${aluno.id}/edit`}>
               <FaEdit size={16} />
             </Link>
-            <Link to={`/student/${aluno.id}/delete`}>
+            <Link onClick={handleDeleteAsk} to={`/student/${aluno.id}/delete`}>
               <FaWindowClose size={16} />
             </Link>
+
+            <FaExclamation
+              size={16}
+              display="none"
+              cursor="pointer"
+              onClick={(e) => handleDelete(e, aluno.id, index)}
+            />
           </div>
         ))}
       </StudentContainer>
